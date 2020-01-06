6e38a0fa5ff59ca7a6e9204e273bb8dd99eb9b76
Gieger <4070685@vtsd.com>
2020-01-06 11:05:51 -0500

    Task1

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,6 +1,8 @@
 /* Put code for the task below */
-function functionName(){
-	alert("Run My Stuff");
-	
-	
+function magicButton(){
+	document.getElementById("buttonTask1").style.display = "inline";
+}
+
+function disappear(){
+	document.getElementById("imageTask1").style.display = "none";
 }
